9cc412e77b21852dc52b04fdfae8bfb0ad3ddcf4
Dimitri9988 <142217656+Dimitri9988@users.noreply.github.com>
2023-09-23 18:32:40 +0200

    Update CategoryDetailDto.java

--- src/main/java/ch/csbe/backendlb/resource/category/categorydto/CategoryDetailDto.java
+++ src/main/java/ch/csbe/backendlb/resource/category/categorydto/CategoryDetailDto.java
@@ -7,20 +7,18 @@ import lombok.Data;
 
 //DTO für die Detailansicht einer Kategory
 @Data
-@Schema(description = "DTO zur Detailansicht einer Kategory")
+@Schema (description = "DTO zur Detailansicht einer Kategory")
 public class CategoryDetailDto {
     @NotNull
     @Schema(description = "einmalige Id zum identifiziren einer Kategory")
     public Long id;
     @NotNull
-    @Schema(description = "aktivitäts status einer Kategory")
-    public String active;
+    @Schema (description = "aktivitäts status einer Kategory")
+    public  String active;
     @NotNull
-    @Schema(description = "Name einer Kategory")
+    @Schema (description = "Name einer Kategory")
     public String name;
     @NotNull
-    @Schema(description = "Produkte einer Kategory")
+    @Schema (description = "Produkte einer Kategory")
     private ProductShowDto product;
 }
-
-
